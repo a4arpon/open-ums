666fb04e939147bf51d0396a918f85f1730dc2ac
Arpon <a4arpon@gmail.com>
2023-09-21 15:03:33 +0600

    aliases configed for import option

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,5 +1,7 @@
-const Card = () => {
-  return <div>Card</div>
+import { ReactChildren } from '../../interfaces/Shared-Interface'
+
+const Card: React.FC<ReactChildren> = ({ children }) => {
+  return <div className="bg-black text-white">{children}</div>
 }
 
 export default Card
---
+++ src/layouts/App.tsx
@@ -0,0 +1,14 @@
+import Button from '@/components/shared/Button'
+import Card from '../components/shared/Card'
+
+const App = () => {
+  return (
+    <div className="p-10">
+      <Card>
+        <Button />
+      </Card>
+    </div>
+  )
+}
+
+export default App

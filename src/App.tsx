8eb8f2fc2d81f874b1f662cd5894ff3983ef43cc
Arpon <a4arpon@gmail.com>
2023-09-21 14:11:14 +0600

    git init

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="mt-10 border-l-0 px-2 text-3xl shadow-md">App</div>
+}
+
+export default App
---
+++ src/interfaces/Shared-Interface.ts
@@ -0,0 +1,5 @@
+export interface ReactChildren {
+  children: React.ReactNode
+}
+
+export interface CardProps extends ReactChildren {}

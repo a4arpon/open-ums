1b704832a073d82f8b377d58282070d794c9e42c
Arpon <a4arpon@gmail.com>
2023-09-25 12:25:19 +0600

    helment added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,20 @@ import ProfileCard from '@/components/others/ProfileCard'
 import Button from '@/components/shared/Button'
 import Loader from '@/components/shared/Loader'
 import SlicedData from '@/lib/pagination'
+import { Helmet } from 'react-helmet-async'
 
 const Home = () => {
   const { contacts, isLoading, reSlice, pagination } = SlicedData()
   if (!isLoading) {
     return (
       <>
+        <Helmet>
+          <title>OPEN CONTACT DB</title>
+          <meta
+            name="description"
+            content="The biggest dummy contact database."
+          />
+        </Helmet>
         <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {contacts &&
             contacts?.map((item) => <ProfileCard {...item} key={item.id} />)}
--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -1,12 +1,15 @@
 import Store from '@/redux/Store'
 import Routes from '@/routes/Router'
+import { HelmetProvider } from 'react-helmet-async'
 import { Provider as ReduxProvider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 const Provider = () => {
   return (
-    <ReduxProvider store={Store}>
-      <RouterProvider router={Routes} />
-    </ReduxProvider>
+    <HelmetProvider>
+      <ReduxProvider store={Store}>
+        <RouterProvider router={Routes} />
+      </ReduxProvider>
+    </HelmetProvider>
   )
 }
 
